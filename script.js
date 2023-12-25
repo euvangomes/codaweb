12aac9f3619d133885c541727a935d202415ef4d
lelo-gael <vanderson588silva@gmail.com>
2023-12-25 04:05:10 -0300

    infernyy

--- script.js
+++ script.js
@@ -26,8 +26,49 @@ function card1() {
     imagens[indice - 1].style.opacity = "1";
     // Colocar a imagem atual acima das outras
     imagens[indice - 1].style.zIndex = "2";
-  }, 3000);
+  }, 3500);
 }
 
 // Chamar a função
 card1();
+
+function navBarToggle(){
+  const navBar = document.querySelector("nav"),
+       menuBtns = document.querySelectorAll(".menu"),
+       blur = document.querySelector(".blur");
+
+     menuBtns.forEach((menuBtn) => {
+       menuBtn.addEventListener("click", () => {
+         navBar.classList.toggle("open");
+       });
+     });
+
+     blur.addEventListener("click", () => {
+       navBar.classList.remove("open");
+     });
+}
+
+navBarToggle();
+
+function texto (){
+  var texto = document.getElementById ("#area-de-texto");
+
+// add an event listener for input event
+texto.addEventListener ("input", function () {
+  // reset the height to the minimum
+  textarea.style.height = "auto";
+  
+  // get the computed style of the textarea
+  var style = window.getComputedStyle (texto);
+  
+  // calculate the height needed to fit the content
+  var height = parseInt (style.getPropertyValue ("border-top-width")) +
+               parseInt (style.getPropertyValue ("border-bottom-width")) +
+               textarea.scrollHeight;
+  
+  // set the height of the textarea
+  textarea.style.height = height + "px";
+});
+}
+
+texto();
